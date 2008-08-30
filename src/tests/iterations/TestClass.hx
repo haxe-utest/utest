077b18816fb01798139369e447e57c836a28d574
franco.ponticelli <franco.ponticelli@5b575154-3555-0410-9e21-f35c1fb8ffa6>
2008-08-30 12:02:20 +0000

    git-svn-id: https://utest.googlecode.com/svn/trunk@9 5b575154-3555-0410-9e21-f35c1fb8ffa6

---
+++ src/tests/iterations/TestClass.hx
@@ -0,0 +1,44 @@
+package tests.iterations;
+
+import utest.Assert;
+
+class TestClass {
+	public var tested       : Bool;
+	public var donesetup    : Bool;
+	public var doneteardown : Bool;
+
+	public function new() {
+		tested       = false;
+		doneteardown = false;
+		donesetup    = false;
+	}
+
+	public function setup() {
+		donesetup = true;
+	}
+
+	public function teardown() {
+		doneteardown = true;
+	}
+
+	public function test() {
+		tested = true;
+	}
+
+	public function throwError() {
+		throw "error";
+	}
+
+	public function assertTrue() {
+		Assert.isTrue(true);
+	}
+
+	public function assertAsync() {
+		var async = Assert.createAsync(function() Assert.isTrue(true));
+#if (flash || js)
+		haxe.Timer.delay(async, 50);
+#else
+		async();
+#end
+	}
+}
