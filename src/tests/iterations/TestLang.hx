298b8da1ad55cc5f7e25ad60b08c03f1639e600a
franco.ponticelli <franco.ponticelli@5b575154-3555-0410-9e21-f35c1fb8ffa6>
2009-05-08 11:30:20 +0000

    git-svn-id: https://utest.googlecode.com/svn/trunk@17 5b575154-3555-0410-9e21-f35c1fb8ffa6

---
+++ src/tests/iterations/TestLang.hx
@@ -0,0 +1,45 @@
+﻿package tests.iterations;
+
+import utest.ui.text.TraceReport;
+
+class TestLang {
+	static function main() {
+		var runner = new Runner();
+		runner.addCase(new tests.lang.AnonymousObject());
+//		runner.addCase(new tests.lang.ArraySyntax());
+		runner.addCase(new tests.lang.Bitwise());
+		runner.addCase(new tests.lang.Callback());
+		runner.addCase(new tests.lang.Casts());
+		// reformulate some of this tests since other tests (from ClassDefAccess) can modify the results
+		runner.addCase(new tests.lang.DynamicFunction());
+		runner.addCase(new tests.lang.ClassDefAccess());
+		runner.addCase(new tests.lang.ClassInheritance());
+		runner.addCase(new tests.lang.CodeBlocks());
+		runner.addCase(new tests.lang.DynamicClass());
+		runner.addCase(new tests.lang.EnumAccess());
+		runner.addCase(new tests.lang.EnumSyntax());
+		runner.addCase(new tests.lang.EqualityOperators());
+// problem with doc
+//		runner.addCase(new tests.lang.Extensions());
+		runner.addCase(new tests.lang.ForAccess());
+		runner.addCase(new tests.lang.IfAccess());
+		runner.addCase(new tests.lang.InterfaceAccess());
+		runner.addCase(new tests.lang.IntIteratorAccess());
+#if php
+		runner.addCase(new tests.lang.NativeString());
+		runner.addCase(new tests.lang.NativeArray());
+		runner.addCase(new tests.lang.PhpDollarEscape());
+		runner.addCase(new tests.lang.PhpReservedWords());
+#end
+		runner.addCase(new tests.lang.PrivateClassAccess());
+		runner.addCase(new tests.lang.PropertyAccess());
+		runner.addCase(new tests.lang.SwitchCaseAccess());
+		runner.addCase(new tests.lang.TryCatch());
+// problem with doc
+//		runner.addCase(new tests.lang.TypedefAccess());
+		runner.addCase(new tests.lang.UnusualConstructs());
+		runner.addCase(new tests.lang.WhileAccess());
+		var report = new TraceReport(runner);
+		runner.run();
+	}
+}
