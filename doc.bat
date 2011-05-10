ef3b64522109fbd2228ea1f0076d1148c1d5fe2f
franco.ponticelli <franco.ponticelli@5b575154-3555-0410-9e21-f35c1fb8ffa6>
2011-05-10 11:40:52 +0000

    updated doc for release 1.0.0
    git-svn-id: https://utest.googlecode.com/svn/trunk@78 5b575154-3555-0410-9e21-f35c1fb8ffa6

--- doc.bat
+++ doc.bat
@@ -1,3 +1,3 @@
 cd doc
-chxdoc.exe -f flash -f haxe -f neko -f php -f test --templateDir=D:\Projects\caffeine-hx\projects\chxdoc\templates\default --installTemplate=true --showPrivateClasses=false --showPrivateTypedefs=false --showPrivateEnums=false --showPrivateMethods=false --showPrivateVars=false --generateTodoFile=true --showTodoTags=true --ignoreRoot=true --title=uTest doc.neko.xml,neko doc.php.xml,php doc.f9.xml,flash9 doc.f8.xml,flash8 doc.f7.xml,flash7 doc.f6.xml,flash6
+chxdoc.exe -f flash -f haxe -f neko -f php -f test --installTemplate=true --showPrivateClasses=false --showPrivateTypedefs=false --showPrivateEnums=false --showPrivateMethods=false --showPrivateVars=false --generateTodoFile=true --showTodoTags=true --ignoreRoot=true --title=uTest doc.neko.xml,neko doc.php.xml,php doc.f9.xml,flash9 doc.f8.xml,flash8 doc.f7.xml,flash7 doc.f6.xml,flash6
 cd ..
