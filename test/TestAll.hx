e662cd4a71f9274d459396361609c49ce248c52e
Alexander Kuzmenko <alex@stablex.ru>
2018-09-16 00:23:55 +0300

    ITest requires Haxe 3.4+

--- test/TestAll.hx
+++ test/TestAll.hx
@@ -4,17 +4,19 @@ import utest.ui.Report;
 import utest.TestResult;
 
 class TestAll {
+  #if (haxe_ver >= "3.4.0")
   static var testITest:TestITest = new TestITest();
+  #end
 
   public static function addTests(runner : Runner) {
     runner.addCase(new utest.TestAssert());
     runner.addCase(new utest.TestDispatcher());
     #if (haxe_ver >= "3.4.0")
     runner.addCase(new utest.TestAsync());
+    runner.addCase(testITest);
     #end
     runner.addCase(new utest.TestIgnored());
     runner.addCase(new utest.TestRunner());
-    runner.addCase(testITest);
   }
 
   public static function main() {
@@ -27,11 +29,13 @@ class TestAll {
     // get test result to determine exit status
     var r:TestResult = null;
     runner.onProgress.add(function(o){ if (o.done == o.totals) r = o.result;});
+    #if (haxe_ver >= "3.4.0")
     runner.onComplete.add(function(runner) {
       if(testITest.teardownClassRunning) {
         throw 'TestITest: missed teardownClass() async completion.';
       }
     });
+    #end
     runner.run();
   }
 
