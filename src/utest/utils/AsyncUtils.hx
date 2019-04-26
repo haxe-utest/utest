86dc3e592481d73271415171e9913b39fff279da
Andy Li <andy@onthewings.net>
2019-04-26 10:22:41 +0800

    avoid "async" Python keyword
    Haxe 3.4.7 doesn't rename the argument.

--- src/utest/utils/AsyncUtils.hx
+++ src/utest/utils/AsyncUtils.hx
@@ -1,7 +1,7 @@
 package utest.utils;
 
 class AsyncUtils {
-	static public inline function orResolved(async:Null<Async>):Async {
-		return async == null ? Async.getResolved() : async;
+	static public inline function orResolved(_async:Null<Async>):Async {
+		return _async == null ? Async.getResolved() : _async;
 	}
 }
