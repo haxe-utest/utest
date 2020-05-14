5d3cc112b1eda665fba148df873fad55ab6361d2
Aleksandr Kuzmenko <alex@stablex.ru>
2020-05-14 19:48:42 +0300

    update submit.sh

--- submit.sh
+++ submit.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 rm utest.zip
-zip -r utest.zip hxml src test haxelib.json README.md CHANGELOG.md -x "*/\.*"
+zip -r utest.zip hxml src test haxelib.json extraParams.hxml README.md CHANGELOG.md -x "*/\.*"
 haxelib submit utest.zip
