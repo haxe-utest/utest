489eac4bec083ae854ea4b5271a1bd3358afefd4
Josh Tynjala <joshtynjala@bowlerhat.dev>
2021-02-24 07:52:49 -0800

    PlainTextReport: exit code for Adobe AIR application (#108)

--- src/utest/ui/text/PlainTextReport.hx
+++ src/utest/ui/text/PlainTextReport.hx
@@ -164,6 +164,8 @@ class PlainTextReport implements IReport<PlainTextReport> {
       #if (haxe_ver >= 4.0) js.Syntax.code #else untyped __js__ #end('phantom').exit(result.stats.isOk ? 0 : 1);
     if(#if (haxe_ver >= 4.0) js.Syntax.code #else untyped __js__ #end('typeof process != "undefined"'))
       #if (haxe_ver >= 4.0) js.Syntax.code #else untyped __js__ #end('process').exit(result.stats.isOk ? 0 : 1);
+#elseif air
+    flash.desktop.NativeApplication.nativeApplication.exit(result.stats.isOk ? 0 : 1);		
 #elseif (flash && exit)
       if(flash.system.Security.sandboxType == "localTrusted") {
         var delay = 5;
