d547a6df536e9b2adad2a6c0013968e768eb3554
AlexHaxe <Alexander.Blum@gmail.com>
2022-01-18 22:27:59 +0100

    trying to fix line breaks

--- src/utest/ui/common/PackageResult.hx
+++ src/utest/ui/common/PackageResult.hx
@@ -4,152 +4,152 @@ import utest.Assertation;
 import utest.TestResult;
 
 class PackageResult {
-	public var packageName(default, null) : String;
-	public var isEmpty(default,null):Bool = true;
-	var classes : Map<String, ClassResult>;
-	var packages : Map<String, PackageResult>;
+  public var packageName(default, null) : String;
+  public var isEmpty(default,null):Bool = true;
+  var classes : Map<String, ClassResult>;
+  var packages : Map<String, PackageResult>;
 
-	public var stats(default, null) : ResultStats;
+  public var stats(default, null) : ResultStats;
 
-	public function new(packageName : String) {
-		this.packageName = packageName;
-		classes = new Map();
-		packages = new Map();
-		stats = new ResultStats();
-	}
+  public function new(packageName : String) {
+    this.packageName = packageName;
+    classes = new Map();
+    packages = new Map();
+    stats = new ResultStats();
+  }
 
-	public function addResult(result : TestResult, flattenPackage : Bool) {
-		isEmpty = false;
-		var pack = getOrCreatePackage(result.pack, flattenPackage, this);
-		var cls = getOrCreateClass(pack, result.cls, result.setup, result.teardown);
-		var fix = createFixture(result.method, result.assertations, result.executionTime);
-		cls.add(fix);
-	}
+  public function addResult(result : TestResult, flattenPackage : Bool) {
+    isEmpty = false;
+    var pack = getOrCreatePackage(result.pack, flattenPackage, this);
+    var cls = getOrCreateClass(pack, result.cls, result.setup, result.teardown);
+    var fix = createFixture(result.method, result.assertations, result.executionTime);
+    cls.add(fix);
+  }
 
-	public function addClass(result : ClassResult) {
-		isEmpty = false;
-		classes.set(result.className, result);
-		stats.wire(result.stats);
-	}
+  public function addClass(result : ClassResult) {
+    isEmpty = false;
+    classes.set(result.className, result);
+    stats.wire(result.stats);
+  }
 
-	public function addPackage(result : PackageResult) {
-		isEmpty = false;
-		packages.set(result.packageName, result);
-		stats.wire(result.stats);
-	}
+  public function addPackage(result : PackageResult) {
+    isEmpty = false;
+    packages.set(result.packageName, result);
+    stats.wire(result.stats);
+  }
 
-	public function existsPackage(name : String) {
-		return packages.exists(name);
-	}
+  public function existsPackage(name : String) {
+    return packages.exists(name);
+  }
 
-	public function existsClass(name : String) {
-		return classes.exists(name);
-	}
+  public function existsClass(name : String) {
+    return classes.exists(name);
+  }
 
-	public function getPackage(name : String) {
-		if (packageName == null && name == "") return this;
-		return packages.get(name);
-	}
+  public function getPackage(name : String) {
+    if (packageName == null && name == "") return this;
+    return packages.get(name);
+  }
 
-	public function getClass(name : String) {
-		return classes.get(name);
-	}
+  public function getClass(name : String) {
+    return classes.get(name);
+  }
 
-	public function classNames(errorsHavePriority = true) : Array<String> {
-		var names = [];
-		for(name in classes.keys())
-			names.push(name);
-		if(errorsHavePriority) {
-			var me = this;
-			names.sort(function(a, b) {
-				var as = me.getClass(a).stats;
-				var bs = me.getClass(b).stats;
-				if(as.hasErrors) {
-					return (!bs.hasErrors) ? -1 : (as.errors == bs.errors ? Reflect.compare(a, b) : Reflect.compare(as.errors, bs.errors));
-				} else if(bs.hasErrors) {
-					return 1;
-				} else if(as.hasFailures) {
-					return (!bs.hasFailures) ? -1 : (as.failures == bs.failures ? Reflect.compare(a, b) : Reflect.compare(as.failures, bs.failures));
-				} else if(bs.hasFailures) {
-					return 1;
-				} else if(as.hasWarnings) {
-					return (!bs.hasWarnings) ? -1 : (as.warnings == bs.warnings ? Reflect.compare(a, b) : Reflect.compare(as.warnings, bs.warnings));
-				} else if(bs.hasWarnings) {
-					return 1;
-				} else {
-					return Reflect.compare(a, b);
-				}
-			});
-		} else {
-			names.sort(function(a, b) {
-				return Reflect.compare(a, b);
-			});
-		}
-		return names;
-	}
+  public function classNames(errorsHavePriority = true) : Array<String> {
+    var names = [];
+    for(name in classes.keys())
+      names.push(name);
+    if(errorsHavePriority) {
+      var me = this;
+      names.sort(function(a, b) {
+        var as = me.getClass(a).stats;
+        var bs = me.getClass(b).stats;
+        if(as.hasErrors) {
+          return (!bs.hasErrors) ? -1 : (as.errors == bs.errors ? Reflect.compare(a, b) : Reflect.compare(as.errors, bs.errors));
+        } else if(bs.hasErrors) {
+          return 1;
+        } else if(as.hasFailures) {
+          return (!bs.hasFailures) ? -1 : (as.failures == bs.failures ? Reflect.compare(a, b) : Reflect.compare(as.failures, bs.failures));
+        } else if(bs.hasFailures) {
+          return 1;
+        } else if(as.hasWarnings) {
+          return (!bs.hasWarnings) ? -1 : (as.warnings == bs.warnings ? Reflect.compare(a, b) : Reflect.compare(as.warnings, bs.warnings));
+        } else if(bs.hasWarnings) {
+          return 1;
+        } else {
+          return Reflect.compare(a, b);
+        }
+      });
+    } else {
+      names.sort(function(a, b) {
+        return Reflect.compare(a, b);
+      });
+    }
+    return names;
+  }
 
-	public function packageNames(errorsHavePriority = true) : Array<String> {
-		var names = [];
-		if (packageName == null) names.push("");
-		for(name in packages.keys())
-			names.push(name);
-		if(errorsHavePriority) {
-			var me = this;
-			names.sort(function(a, b) {
-				var as = me.getPackage(a).stats;
-				var bs = me.getPackage(b).stats;
-				if(as.hasErrors) {
-					return (!bs.hasErrors) ? -1 : (as.errors == bs.errors ? Reflect.compare(a, b) : Reflect.compare(as.errors, bs.errors));
-				} else if(bs.hasErrors) {
-					return 1;
-				} else if(as.hasFailures) {
-					return (!bs.hasFailures) ? -1 : (as.failures == bs.failures ? Reflect.compare(a, b) : Reflect.compare(as.failures, bs.failures));
-				} else if(bs.hasFailures) {
-					return 1;
-				} else if(as.hasWarnings) {
-					return (!bs.hasWarnings) ? -1 : (as.warnings == bs.warnings ? Reflect.compare(a, b) : Reflect.compare(as.warnings, bs.warnings));
-				} else if(bs.hasWarnings) {
-					return 1;
-				} else {
-					return Reflect.compare(a, b);
-				}
-			});
-		} else {
-			names.sort(function(a, b) {
-				return Reflect.compare(a, b);
-			});
-		}
-		return names;
-	}
+  public function packageNames(errorsHavePriority = true) : Array<String> {
+    var names = [];
+    if (packageName == null) names.push("");
+    for(name in packages.keys())
+      names.push(name);
+    if(errorsHavePriority) {
+      var me = this;
+      names.sort(function(a, b) {
+        var as = me.getPackage(a).stats;
+        var bs = me.getPackage(b).stats;
+        if(as.hasErrors) {
+          return (!bs.hasErrors) ? -1 : (as.errors == bs.errors ? Reflect.compare(a, b) : Reflect.compare(as.errors, bs.errors));
+        } else if(bs.hasErrors) {
+          return 1;
+        } else if(as.hasFailures) {
+          return (!bs.hasFailures) ? -1 : (as.failures == bs.failures ? Reflect.compare(a, b) : Reflect.compare(as.failures, bs.failures));
+        } else if(bs.hasFailures) {
+          return 1;
+        } else if(as.hasWarnings) {
+          return (!bs.hasWarnings) ? -1 : (as.warnings == bs.warnings ? Reflect.compare(a, b) : Reflect.compare(as.warnings, bs.warnings));
+        } else if(bs.hasWarnings) {
+          return 1;
+        } else {
+          return Reflect.compare(a, b);
+        }
+      });
+    } else {
+      names.sort(function(a, b) {
+        return Reflect.compare(a, b);
+      });
+    }
+    return names;
+  }
 
-	function createFixture(method : String, assertations : Iterable<Assertation>, executionTime:Float) {
-		var f = new FixtureResult(method, executionTime);
-		for(assertation in assertations)
-			f.add(assertation);
-		return f;
-	}
+  function createFixture(method : String, assertations : Iterable<Assertation>, executionTime:Float) {
+    var f = new FixtureResult(method, executionTime);
+    for(assertation in assertations)
+      f.add(assertation);
+    return f;
+  }
 
-	function getOrCreateClass(pack : PackageResult, cls : String, setup : String, teardown : String) {
-		if(pack.existsClass(cls)) return pack.getClass(cls);
-		var c = new ClassResult(cls, setup, teardown);
-		pack.addClass(c);
-		return c;
-	}
+  function getOrCreateClass(pack : PackageResult, cls : String, setup : String, teardown : String) {
+    if(pack.existsClass(cls)) return pack.getClass(cls);
+    var c = new ClassResult(cls, setup, teardown);
+    pack.addClass(c);
+    return c;
+  }
 
-	function getOrCreatePackage(pack : String, flat : Bool, ref : PackageResult) {
-		if(pack == null || pack == '') return ref;
-		if(flat) {
-			if(ref.existsPackage(pack))
-				return ref.getPackage(pack);
-			var p = new PackageResult(pack);
-			ref.addPackage(p);
-			return p;
-		} else {
-			var parts = pack.split('.');
-			for(part in parts) {
-				ref = getOrCreatePackage(part, true, ref);
-			}
-			return ref;
-		}
-	}
+  function getOrCreatePackage(pack : String, flat : Bool, ref : PackageResult) {
+    if(pack == null || pack == '') return ref;
+    if(flat) {
+      if(ref.existsPackage(pack))
+        return ref.getPackage(pack);
+      var p = new PackageResult(pack);
+      ref.addPackage(p);
+      return p;
+    } else {
+      var parts = pack.split('.');
+      for(part in parts) {
+        ref = getOrCreatePackage(part, true, ref);
+      }
+      return ref;
+    }
+  }
 }
--- src/utest/ui/common/ResultAggregator.hx
+++ src/utest/ui/common/ResultAggregator.hx
@@ -5,108 +5,108 @@ import utest.Runner;
 import utest.TestResult;
 
 class ResultAggregator {
-	var runner : Runner;
-	var flattenPackage : Bool;
-	public var root(default, null) : PackageResult;
+  var runner : Runner;
+  var flattenPackage : Bool;
+  public var root(default, null) : PackageResult;
 
-	public var onStart(default, null) : Notifier;
-	public var onComplete(default, null) : Dispatcher<PackageResult>;
-	public var onProgress(default, null) : Dispatcher<{ done : Int, totals : Int }>;
+  public var onStart(default, null) : Notifier;
+  public var onComplete(default, null) : Dispatcher<PackageResult>;
+  public var onProgress(default, null) : Dispatcher<{ done : Int, totals : Int }>;
 
-	public function new(runner : Runner, flattenPackage = false) {
-		if(runner == null) throw "runner argument is null";
-		this.flattenPackage = flattenPackage;
-		this.runner = runner;
-		runner.onStart.add(start);
-		runner.onProgress.add(progress);
-		runner.onComplete.add(complete);
+  public function new(runner : Runner, flattenPackage = false) {
+    if(runner == null) throw "runner argument is null";
+    this.flattenPackage = flattenPackage;
+    this.runner = runner;
+    runner.onStart.add(start);
+    runner.onProgress.add(progress);
+    runner.onComplete.add(complete);
 
-		onStart = new Notifier();
-		onComplete = new Dispatcher();
-		onProgress = new Dispatcher();
-	}
+    onStart = new Notifier();
+    onComplete = new Dispatcher();
+    onProgress = new Dispatcher();
+  }
 
-	function start(runner : Runner) {
-		checkNonITest();
-		root = new PackageResult(null);
-		onStart.dispatch();
-	}
+  function start(runner : Runner) {
+    checkNonITest();
+    root = new PackageResult(null);
+    onStart.dispatch();
+  }
 
-	function checkNonITest() {
-		var first = null;
-		var total = 0;
-		for(i in 0...runner.length) {
-			var fixture = runner.getFixture(i);
-			if(!fixture.isITest) {
-				total++;
-				if(first == null) {
-					first = Type.getClassName(Type.getClass(fixture.target));
-				}
-			}
-		}
-		if(total > 0) {
-			var baseMsg = 'implement utest.ITest. Non-ITest tests are deprecated. Implement utest.ITest or extend utest.Test.';
-			var msg = switch(total) {
-				case 1: '$first doesn\'t $baseMsg';
-				case 2: '$first and 1 other don\'t $baseMsg';
-				case _: '$first and $total others don\'t $baseMsg';
-			}
-			trace(msg);
-		}
-	}
+  function checkNonITest() {
+    var first = null;
+    var total = 0;
+    for(i in 0...runner.length) {
+      var fixture = runner.getFixture(i);
+      if(!fixture.isITest) {
+        total++;
+        if(first == null) {
+          first = Type.getClassName(Type.getClass(fixture.target));
+        }
+      }
+    }
+    if(total > 0) {
+      var baseMsg = 'implement utest.ITest. Non-ITest tests are deprecated. Implement utest.ITest or extend utest.Test.';
+      var msg = switch(total) {
+        case 1: '$first doesn\'t $baseMsg';
+        case 2: '$first and 1 other don\'t $baseMsg';
+        case _: '$first and $total others don\'t $baseMsg';
+      }
+      trace(msg);
+    }
+  }
 
-	function getOrCreatePackage(pack : String, flat : Bool, ?ref : PackageResult) {
-		if(ref == null) ref = root;
-		if(pack == null || pack == '') return ref;
-		if(flat) {
-			if(ref.existsPackage(pack))
-				return ref.getPackage(pack);
-			var p = new PackageResult(pack);
-			ref.addPackage(p);
-			return p;
-		} else {
-			var parts = pack.split('.');
-			for(part in parts) {
-				ref = getOrCreatePackage(part, true, ref);
-			}
-			return ref;
-		}
-	}
+  function getOrCreatePackage(pack : String, flat : Bool, ?ref : PackageResult) {
+    if(ref == null) ref = root;
+    if(pack == null || pack == '') return ref;
+    if(flat) {
+      if(ref.existsPackage(pack))
+        return ref.getPackage(pack);
+      var p = new PackageResult(pack);
+      ref.addPackage(p);
+      return p;
+    } else {
+      var parts = pack.split('.');
+      for(part in parts) {
+        ref = getOrCreatePackage(part, true, ref);
+      }
+      return ref;
+    }
+  }
 
-	function getOrCreateClass(pack : PackageResult, cls : String, setup : String, teardown : String) {
-		if(pack.existsClass(cls)) return pack.getClass(cls);
-		var c = new ClassResult(cls, setup, teardown);
-		pack.addClass(c);
-		return c;
-	}
+  function getOrCreateClass(pack : PackageResult, cls : String, setup : String, teardown : String) {
+    if(pack.existsClass(cls)) return pack.getClass(cls);
+    var c = new ClassResult(cls, setup, teardown);
+    pack.addClass(c);
+    return c;
+  }
 
-	function createFixture(result : TestResult) {
-		var f = new FixtureResult(result.method, result.executionTime);
-		for(assertation in result.assertations)
-			f.add(assertation);
-		return f;
-	}
+  function createFixture(result : TestResult) {
+    var f = new FixtureResult(result.method, result.executionTime);
+    for(assertation in result.assertations)
+      f.add(assertation);
+    return f;
+  }
 
-	function progress(e) {
-		root.addResult(e.result, flattenPackage);
-		onProgress.dispatch(e);
-	}
+  function progress(e) {
+    root.addResult(e.result, flattenPackage);
+    onProgress.dispatch(e);
+  }
 
-	function complete(runner : Runner) {
-		if(root.isEmpty) {
-			root.addResult(createNoTestsResult(), false);
-		}
-		onComplete.dispatch(root);
-	}
+  function complete(runner : Runner) {
+    if(root.isEmpty) {
+      root.addResult(createNoTestsResult(), false);
+    }
+    onComplete.dispatch(root);
+  }
 
-	function createNoTestsResult():TestResult {
-		var result = new TestResult();
-		result.pack = '';
-		result.cls = '';
-		result.method = '';
-		result.assertations = new List();
-		var pos = {fileName:'', lineNumber:1, className:'utest.Runner', methodName:'run'};
-		result.assertations.add(Failure('No tests executed.', pos));
-		return result;
-	}
+  function createNoTestsResult():TestResult {
+    var result = new TestResult();
+    result.pack = '';
+    result.cls = '';
+    result.method = '';
+    result.assertations = new List();
+    var pos = {fileName:'', lineNumber:1, className:'utest.Runner', methodName:'run'};
+    result.assertations.add(Failure('No tests executed.', pos));
+    return result;
+  }
 }
